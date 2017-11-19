1ef3648614fd3c03382fb3f5798beb8992a65007
Roderick <roderickfung@users.noreply.github.com>
2017-11-18 17:45:26 -0800

    feat: router working and connected

--- containers/app.js
+++ containers/app.js
@@ -1,18 +1,17 @@
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import {HomePage} from './home';
-import {SpeakerPage} from './speaker-page';
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import Home from './home';
+import SpeakerPage from './speaker-page'
 
-export default class App extends Component {
-    render () {
-        return (
-            <div>
-                <Header />
-                <Switch>
-                    <Route exact path='/' component={HomePage} />
-                    <Route path='/speakers' component={SpeakerPage} />
-                </Switch>
-                <Footer />
-            </div>
-        )
-    }
+class App extends React.Component {
+  render () {
+    return (
+      <Switch>
+        <Route path='/' component={Home} />
+        <Route path='/speakers' component={SpeakerPage} />
+      </Switch>
+    );
+  }
 }
+    
+export default App;
