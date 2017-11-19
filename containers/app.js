9f30ebc4c8579d5a970532d9199a007871096fea
Brandon Lam <brandonywlam@gmail.com>
2017-11-18 16:42:38 -0800

    create speaker page container, create speaker component

--- containers/app.js
+++ containers/app.js
@@ -1,7 +1,13 @@
 import React, {Component} from 'react';
+import SpeakerPage from './speaker-page';
 
 export default class App extends Component {
     render () {
-        return <p>This is my new react app</p>
+        return(
+            <div>
+            <p>This is my new react app</p>
+                <SpeakerPage></SpeakerPage>
+            </div>
+        )
     }
 }
