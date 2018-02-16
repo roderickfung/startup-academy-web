94f16f9c003913c987c565bc38110ee721afb794
Brandon Lam <brandonywlam@gmail.com>
2018-02-15 20:10:40 -0800

    add agenda page

---
+++ data/agenda.js
@@ -0,0 +1,153 @@
+const data = {
+	header: {
+		backgroundImage: "https://static.wixstatic.com/media/3d3337_45863f3c1d7347b48529c2c031da4270~mv2.jpg/v1/fill/w_1248,h_911,al_c,q_85/3d3337_45863f3c1d7347b48529c2c031da4270~mv2.webp",
+		title: 'SCHEDULE',
+		details: null
+	},
+	timelines: [
+		{
+			date: 'DAY 1 / March 17th',
+			location: 'Venture Labs (Downtown)',
+			agenda: [
+				{
+					timestamp: '8:30',
+					details: [
+						{normal_text: 'Registration / Meet Your Team'}
+					]
+				},
+				{
+					timestamp: '9:00',
+					details: [
+						{normal_text:'Team Work Time Prompt'}
+					]
+				},
+				{
+					timestamp: '10:00',
+					details: [
+						{underlined_text: 'Workshop 1:'},
+						{normal_text: 'Ideation Workshop'}
+					]
+				},
+				{
+					timestamp: '11:10',
+					details: [
+						{underlined_text: 'Workshop 2:'},
+						{normal_text: 'Business Development Workshop'}
+					]
+				},
+				{
+					timestamp: '12:10',
+					details: [
+						{normal_text: 'Lunch Break'}
+					]
+				},
+				{
+					timestamp: '13:10',
+					details: [
+						{underlined_text: 'Workshop 3:'},
+						{normal_text: 'Prototyping & User Experience'}
+					]
+				},
+				{
+					timestamp: '14:10',
+					details: [
+						{normal_text: 'Meet Your Mentors'}
+					]
+				},
+				{
+					timestamp: '14:30',
+					details: [
+						{underlined_text: 'Work Time + Mentorship Sessions'},
+						{normal_text: '2:30PM - 5:00PM - Work Time'},
+						{normal_text: '5:00PM - 7:00PM - Mentorship'},
+						{normal_text: '7:00PM - 9:00PM - Work Time'}
+					]
+				},
+				{
+					timestamp: '18:00',
+					details: [
+						{normal_text: 'Dinner (Catered) Pick Up'}
+					]
+				}
+			],
+		},
+		{
+			date: 'Day 2 / March 18th',
+			location: 'Venture Labs (Downtown)',
+			agenda: [
+				{
+					timestamp: '10:30',
+					details: [
+						{underlined_text: 'Mentorship Sessions:'},
+						{normal_text: 'Pitching'}
+					]
+				},
+				{
+					timestamp: '15:00',
+					details: [
+						{normal_text: 'Event Opening'}
+					]
+				},
+				{
+					timestamp: '15:15',
+					details: [
+						{underlined_text: 'Expo Competition:'},
+						{normal_text: '12 Min Rotations'}
+					]
+				},
+				{
+					timestamp: '17:15',
+					details: [
+						{normal_text: 'Judge Evaluations (5:15PM) + Winning Teams Announcement (6:00PM)'}
+					]
+				}
+			]
+		},
+		{
+			date: 'DAY 3 / March 30th',
+			location: 'SFU Downtown Vancouver',
+			agenda: [
+				{
+					timestamp: '13:00',
+					details: [
+						{normal_text:'Opening Ceremony'}
+					]
+				},
+				{
+					timestamp: '13:15',
+					details: [
+						{normal_text: 'Keynote Speakers'}
+					]
+				},
+				{
+					timestamp: '14:30',
+					details: [
+						{normal_text:'Entrepreneurship Panel:'},
+						{normal_text:'Q&A'}
+					]
+				},
+				{
+					timestamp: '15:45',
+					details: [
+						{underlined_text: 'Hackathon Pitches:'},
+						{normal_text: 'Top 3 Teams'}
+					]
+				},
+				{
+					timestamp: '16:15',
+					details: [
+						{normal_text: 'Judge Evaluations'}
+					]
+				},
+				{
+					timestamp: '16:30',
+					details: [
+						{normal_text: 'Networking Session'}
+					]
+				}
+			]
+		}
+	]
+}
+
+export default data;
