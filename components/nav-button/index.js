5ceefd4e13a3354b7db5ac34d427bd7c63d746f7
Brandon Lam <brandonywlam@gmail.com>
2018-02-16 16:17:40 -0800

    edit component nav-button: add onHover underline

--- components/nav-button/index.js
+++ components/nav-button/index.js
@@ -4,12 +4,37 @@ import {Link} from 'react-router-dom';
 import './nav-button.css';
 
 export default class NavButton extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isHovered: false
+    }
+  }
+
+  setHoverTrue = () => {
+    this.setState({isHovered:true});
+  }
+
+  setHoverFalse = () => {
+    this.setState({isHovered:false});
+  }
+
   computeButton() {
     if (this.props.route) {
-      return <Link className="nav-button" to={this.props.route}><span className="nav-button-title">{this.props.title}</span></Link>
+      return (
+        <Link className="nav-button" to={this.props.route} onMouseEnter={this.setHoverTrue} onMouseLeave={this.setHoverFalse}>
+          <span className="nav-button-title">{this.props.title}</span>
+          <span className={this.state.isHovered ? "nav-button-underline" : ""}></span>
+        </Link>
+      )
     } else {
       console.log(this.props)
-      return <a className="nav-button" rel="external" target="_self" href={this.props.redirectUrl}><span className="nav-button-title">{this.props.title}</span></a>
+      return (
+        <a className="nav-button" rel="external" target="_self" href={this.props.redirectUrl} onMouseEnter={this.setHoverTrue} onMouseLeave={this.setHoverFalse}>
+          <span className="nav-button-title">{this.props.title}</span>
+          <span className={this.state.isHovered ? "nav-button-underline" : ""}></span>
+        </a>
+      )
     }
   }
   render() {
